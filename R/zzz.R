d3ad5b2a62faf748af9dfe0988607cc0ec2003dd
Benjamin Becker <beckerbz@user.hu-berlin.de>
2018-09-28 17:30:48 +0200

    bugfix

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 
 ## check for sqlite3
 onLoad <- function(){
-  sqlite_test <- try(shell(cmd = "sqlite3", mustWork = TRUE, intern = TRUE))
-  if(class(sqlite_test) == "try-error") stop("SQLite3 must be installed and correct PATH must be specified for eatDB to create data bases.
-                                             Usage of existing data bases works without SQLite3.")
+  sqlite_test <- tryCatch(shell(cmd = "sqlite2", mustWork = TRUE, intern = TRUE))
+  if(!is.null(attributes(sqlite_test))) warning("SQLite3 must be installed and correct PATH must be specified for eatDB to create data bases. \n Usage of existing data bases works without SQLite3.")
 }
